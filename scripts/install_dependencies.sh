12fd8dd97ee8d1578d95f965b85f98b16a611790
kumar488@purdue.edu <ikumar@trustrelate.com>
2024-10-27 20:28:03 -0400

    fixed cd destinations

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ sudo apt-get update -y
 sudo apt-get install -y nodejs npm
 
 # Navigate to the application directory
-cd /home/ec2-user/myprogram
+cd /home/ec2-user/461-Phase2
 
 # Install dependencies
 ./run install
