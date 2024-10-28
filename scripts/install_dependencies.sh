2c8418647d36aa0e75bc28e7df9eb667bb1ecaa0
kumar488@purdue.edu <ikumar@trustrelate.com>
2024-10-27 21:36:17 -0400

    tests

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -9,9 +9,9 @@ sudo yum -y update
 sudo yum -y install ruby wget python3-pip
 
 # Navigate to the application directory
-cd /home/ec2-user/461-Phase2
+# cd /home/ec2-user/461-Phase2
 
-# Install dependencies
-chmod +x run
-./run install
+# # Install dependencies
+# chmod +x run
+# ./run install
 
